24fd4c193db1ed408a99521b9fb6521fc04bf4cb
Rafael Oviedo <oviedo.rp@gmail.com>
2022-04-23 10:35:15 -0300

    new(router, store): adding router and store main structures

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { createStore } from "vuex";
+import axios from "axios";
+
+const store = createStore({
+    state: {
+        podcasts: [],
+    },
+    mutations: {},
+    actions: {},
+    getters: {},
+    modules: {}
+});
+
+export default store;
