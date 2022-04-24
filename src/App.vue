b3cde327125ead4fae8360f3457f766d276c535d
Rafael Oviedo <oviedo.rp@gmail.com>
2022-04-24 14:29:43 -0300

    update(App.vue): hidding scroll bar generally

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <NavBar />
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from '../src/components/NavBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    NavBar,
+  },
+  data() {
+    return {
+    }
+  },
 }
+
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: Poppins-Black;
+}
+
+html::-webkit-scrollbar {
+  display: none;
 }
 </style>
