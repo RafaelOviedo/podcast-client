5e13aa5c05af7941a8df4101e8c1c4b9bed76dcc
Rafael Oviedo <oviedo.rp@gmail.com>
2022-04-24 14:31:39 -0300

    update(store): adding actions and mutation for hero data

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,20 @@ import axios from "axios";
 
 const store = createStore({
     state: {
+        heroContent: [],
         podcasts: [],
     },
-    mutations: {},
-    actions: {},
+    mutations: {
+        setHeroContent(state, payload) {
+            state.heroContent = payload;
+        }
+    },
+    actions: {
+        async getHeroContent({ commit }) {
+            const response = await axios.get("http://localhost:8888/wordpress/wp-json/wp/v2/pages");
+            commit("setHeroContent", response.data[0].acf);
+        }
+    },
     getters: {},
     modules: {}
 });
