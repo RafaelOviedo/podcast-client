3e7e60115c32f43c4ac82d1991c97a43811aa2c1
Rafael Oviedo <oviedo.rp@gmail.com>
2022-04-23 23:30:25 -0300

    update(router): adding routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,35 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../views/HomePage';
+import About from '../views/About';
+import Episodes from '../views/Episodes';
+import Blog from '../views/Blog';
+import Contact from '../views/Contact';
 
 const routes = [
     {
-        path: '/home',
+        path: '/',
         name: 'HomePage',
         component: HomePage
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About
+    },
+    {
+        path: '/episodes',
+        name: 'Episodes',
+        component: Episodes
+    },
+    {
+        path: '/blog',
+        name: 'Blog',
+        component: Blog
+    },
+    {
+        path: '/contact',
+        name: 'Contact',
+        component: Contact
     },
 ]
 
